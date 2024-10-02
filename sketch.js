93ea15f0f9e7ae009095929eb053f30271857a33
KEA KAMIYA <KSK23A@FSU.EDU>
2024-10-01 20:59:59 -0400

    ITWORKS

--- sketch.js
+++ sketch.js
@@ -1,18 +1,51 @@
 let mySoundOne;
 let loopStart = 5;
 let loopDuration = 2;
+let mic;
 
 function preload() {
   mySoundOne = loadSound("assets/Fine Arts Building.mp3");
 }
 
 function setup() {
-  let cnv = createCanvas(100,100);
-  cnv.mousePressed(canvasPressed) ;
-  background(220);
+  let cnv = createCanvas(400,400);
+  background(200);
+  
   text('tap here to play',10,20);
+  mic = new p5.AudioIn();
+  mic.start()
+
+  cnv.mousePressed(canvasPressed);
+
+  console.log("Mic Started")
+}
+
+
 
 function canvasPressed() {
-  mySoundOne.play();
+  if (mySoundOne.isPlaying()) {
+    mySoundOne.stop();
+  } else {
+    mySoundOne.loop();
+  }
 }
-}
+  
+
+
+function draw() {
+  let volume = mic.getLevel();
+  console.log ("mic Volume" + volume);
+
+  let playaudio = map(volume, 0, 1, 20, 100)
+  console.log ("mapped volume" + playaudio)
+
+  if (playaudio > 21) {
+    mySoundOne.loop();} 
+    else {
+      if (mySoundOne.isPlaying()){
+        mySoundOne.stop();
+
+        //ellipse(width/2, height/2, playaudio, playaudio);
+    }
+    }
+  }
