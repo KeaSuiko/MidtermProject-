42aaedc768f1af08116d559c58f979612ece12e4
KEA KAMIYA <KSK23A@FSU.EDU>
2024-10-09 09:55:02 -0400

    using the mic

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
 let outputDevice; 
 let cc = 0;
+let mic;
   
 
 function setup() {
     let cnv = createCanvas(400,400);
+    cnv.mousePressed(userStartAudio);
+    textAlign(CENTER);
+    mic = new p5.AudioIn();
+    mic.start();
+
     background(200);
     text('tap here to play',10,20);
-
-  // try to setup WebMIDI and set outputDevice to the first available device
+  
   navigator.requestMIDIAccess()
   .then(function(midiAccess) {
     // Get the first available MIDI output port
@@ -16,32 +21,25 @@ function setup() {
   })
   .catch(function(error) {
     console.error("Error accessing MIDI devices");
-    //console.error(error);
-    
-  //mic = new p5.AudioIn();
-  //mic.start()
-  //cnv.mousePressed(canvasPressed);
-
-  //console.log("Mic Started")
   });
+  
 }
-
 
 function draw() {}
 
 
-// if a key is pressed, send a 1 second long note
 function keyPressed() {
     // Send a MIDI note on message
     console.log(cc);
 
-    outputDevice.send([176, cc, 127]); // Note on, middle C, velocity 127
+    outputDevice.send([176, cc, 127]); //turning on the path that cues the different sounds 
 
     // After a delay, send a MIDI note off message
     setTimeout(function() {
-      outputDevice.send([176, cc, 0]); // Note off, middle C
-    }, 1000); // 1000 milliseconds == 1 second
-cc= (cc+1) % 3; 
+      outputDevice.send([176, cc, 0]);
+    }, 1000); 
+cc= (cc+1) % 3; //loop through the sounds to cue different audios 
+
 }
 
 
